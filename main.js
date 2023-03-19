78b4b116ca587eb5199c6da2bf45fbce5a41bf1d
Kerem Mert <kerem37mert@yahoo.com>
2023-03-19 16:11:32 +0300

    porject complete

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ playButton.addEventListener("click", () => {
 });
 
 //Character position
-let positionY = 0;
-let positionX = 0;
+
 
 const playGame = () => {
     const charDom = document.createElement("img");
@@ -36,21 +35,49 @@ const playGame = () => {
     charDom.style.bottom = "0";
     gameDom.append(charDom);
 
+    let goldX = 720;
+    let goldY = 450;
+
+    const goldDom = document.createElement("img");
+    goldDom.setAttribute("src","img/gold.png");
+    goldDom.style.width = "80px";
+    goldDom.style.height = "50px";
+    goldDom.style.position = "absolute";
+    goldDom.style.left = goldX + "px";
+    goldDom.style.bottom= goldY + "px";
+    gameDom.append(goldDom);
+
+
+    let positionY = 0;
+    let positionX = 0;
+
     window.addEventListener("keydown", (event) => {
         if(event.keyCode === 37){
-            positionX -= 80;
+            if(positionX != 0) {
+                positionX -= 80;
+            }
         }
         else if(event.keyCode === 38){
-            positionY += 50;
+            if(positionY != 450) {
+                positionY += 50;
+            }
         }
         else if(event.keyCode === 39){
-            positionX += 80;
+            if(positionX != 720) {
+                positionX += 80;
+            }
         }
         else if(event.keyCode === 40){
-            positionY -= 50;
+            if(positionY != 0) {
+                positionY -= 50;
+            }
         }
 
         charDom.style.left = positionX + "px";
         charDom.style.bottom = positionY + "px";
+
+        if(positionX == goldX && positionY == goldY) {
+            alert("Tebrikler");
+        }
     });
 }
